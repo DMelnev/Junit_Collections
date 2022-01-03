3f5ae64c5b2be46e1995f58296ea663c9613af09
DMelnev <59581747+DMelnev@users.noreply.github.com>
2022-01-03 19:18:30 +0300

    realised linkedList.add(obj.index)

---
+++ Lessons/src/main/java/CarLinkedList.java
@@ -0,0 +1,86 @@
+public class CarLinkedList implements CarList {
+
+    private Node first;
+    private Node last;
+    private int size = 0;
+
+    @Override
+    public Car get(int index) {
+        return null;
+    }
+
+    @Override
+    public void add(Car car) {
+        if (size == 0) {
+            first = new Node(null, car, null);
+            last = first;
+        } else {
+            Node secondLast = last;
+            last = new Node(secondLast, car, null);
+            secondLast.next = last;
+        }
+        size++;
+    }
+
+    @Override
+    public void add(Car car, int index) {
+        if (index < 0 || index > size) {
+            throw new IndexOutOfBoundsException();
+        }
+        if (index == size) {
+            add(car);
+            return;
+        }
+        Node nodeNext = getNode(index);
+        Node nodePrevious = nodeNext.previous;
+        Node newNode = new Node(nodePrevious, car, nodeNext);
+        nodeNext.previous = newNode;
+        if (nodePrevious != null) {
+            nodePrevious.next = newNode;
+        } else first = newNode;
+        size++;
+    }
+
+    @Override
+    public boolean remove(Car car) {
+        return false;
+    }
+
+    @Override
+    public boolean removeAt(int index) {
+        if (index < 0 || index > size) {
+            throw new IndexOutOfBoundsException();
+        }
+        return false;
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    private Node getNode(int index) {
+        Node node = first;
+        for (int i = 0; i < index; i++) {
+            node = node.next;
+        }
+        return node;
+    }
+
+    private static class Node {
+        private Node previous;
+        private Car value;
+        private Node next;
+
+        public Node(Node previous, Car value, Node next) {
+            this.previous = previous;
+            this.value = value;
+            this.next = next;
+        }
+    }
+}
