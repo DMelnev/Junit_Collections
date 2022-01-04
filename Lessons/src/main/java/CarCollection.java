452eaac01b4ded847ecd15fe61715108d51f3e77
DMelnev <59581747+DMelnev@users.noreply.github.com>
2022-01-04 23:12:31 +0300

    added interface collection

---
+++ Lessons/src/main/java/CarCollection.java
@@ -0,0 +1,9 @@
+public interface CarCollection {
+    void add(Car car);
+
+    boolean remove(Car car);
+
+    int size();
+
+    void clear();
+}
--- Lessons/src/main/java/CarSet.java
+++ Lessons/src/main/java/CarSet.java
@@ -1,12 +1,16 @@
 /**
  * interface Set collection
+ *
  * @author Melnev Dmitriy
  * @version 2022-01-04
  */
 public interface CarSet {
     boolean add(Car car);
-    boolean remove( Car car);
+
+    boolean remove(Car car);
+
     int size();
+
     void clear();
 
 }
