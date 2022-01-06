6cbac9511065784573bedfb5bf5a6349247f27a7
DMelnev <59581747+DMelnev@users.noreply.github.com>
2022-01-06 22:04:08 +0300

    added interface CarMap

--- Lessons/src/main/java/Main.java
+++ Lessons/src/main/java/Main.java
@@ -1,14 +1,22 @@
+import java.util.Comparator;
 import java.util.Set;
 import java.util.TreeSet;
 
 public class Main {
     public static void main(String[] args) {
-        Set<Integer> numbers = new TreeSet<>();
+        Set<Car> cars = new TreeSet<>(new Comparator<Car>() {
+            @Override
+            public int compare(Car o1, Car o2) {
+//                return o1.getBrand().compareTo(o2.getBrand());
+                return o2.getNumber() - o1.getNumber();
+            }
+        });
         for (int i = 0; i < 100; i++) {
-            numbers.add((int) (Math.random() * 10));
+            int index = (int) (Math.random() * 21);
+            cars.add(new Car("Model" + index, index));
         }
-        for (int dig : numbers) {
-            System.out.println(dig);
+        for (Car car : cars) {
+            System.out.println(car);
         }
     }
 }
